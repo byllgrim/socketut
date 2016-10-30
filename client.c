32cb8343196db8d1c652035d8859d36bf28fa703
byllgrim <robin.a.t.pedersen@gmail.com>
2016-10-30 12:41:57 +0100

    fix client/server compatibility
    The client now successfully connects to the server.
    The message is sent as expected.
    
    The only thing not implemented is the server response.

--- client.c
+++ client.c
@@ -1,30 +1,51 @@
 #include <sys/socket.h>
 #include <netdb.h>
+
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
 
+void die(char *msg) {
+	perror(msg);
+	exit(EXIT_FAILURE);
+}
+
 int main(int argc, char *argv[]) {
-	int sockfd;
+	struct addrinfo hints;
 	struct addrinfo *res;
-	char buf[256];
+	int err;
+	int sockfd;
+	char buf[BUFSIZ+1];
 
-	if (getaddrinfo(NULL, argv[1], 0, &res))
-		perror("getaddrinfo: error");
+	/* construct address */
+	hints.ai_family = AF_UNSPEC;     /* ipv4 or ipv6 */
+	hints.ai_socktype = SOCK_STREAM; /* tcp */
+	hints.ai_protocol = 0;	   /* any protocol */
+	hints.ai_flags = AI_PASSIVE;     /* any address */
+	if ((err = getaddrinfo(NULL, argv[1], &hints, &res))) {
+		fprintf(stderr, "getaddrinfo: %s\n", strerror(err));
+		exit(EXIT_FAILURE);
+	}
 
+	/* create socket */
 	sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-	if (sockfd < 0) perror("failed to open socket");
+	if (sockfd < 0)
+		die("socket");
 
+	/* connect to address */
 	if (connect(sockfd, res->ai_addr, res->ai_addrlen))
-		perror("connect: error");
+		die("connect");
 	freeaddrinfo(res);
 
-	if (send(sockfd, "hey\n", 4, 0) == -1) perror("failed to send");
+	/* send message */
+	if (send(sockfd, "hey\n", 4, 0) < 0)
+		die("send");
 
-	bzero(buf, 256);
-	int n = read(sockfd, buf, 255);
-	if (n < 0) perror("failed to read from socket");
+	/* read reply */
+	bzero(buf, sizeof(buf));
+	if (read(sockfd, buf, BUFSIZ) < 0)
+		die("read");
 	printf("%s\n", buf);
 
 	return 0;
